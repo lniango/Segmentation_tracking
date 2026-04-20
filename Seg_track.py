50e4bcce9062f499a3c4ed0a2169f5d3819b5a5f
Louis Niango <kyo@Host-002.lan>
2026-04-20 23:45:58 +0200

    Plot overlay and contours - watershed segmentation

--- Seg_track.py
+++ Seg_track.py
@@ -9,11 +9,14 @@ from PIL import Image
 import glob
 from skimage.feature import canny
 from skimage.filters import sobel
-from skimage import data,morphology
+from skimage import data
+from skimage.segmentation import watershed
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
 import scipy.ndimage as nd
+from help import mean_dataset, std_dataset
+from skimage.color import label2rgb
 
 path = "DIC-C2DH-HeLa/01"
 save_path = "output"
@@ -58,7 +61,7 @@ def canny_seg(nb=10):
         #plt.show()
 
 # Region segmentation
-def region_seg(nb=10):
+def watershed_seg(nb=10, k=1):
     for i in range(nb):
         img_read = cv.imread(images[i])
         img_read = np.array(cv.cvtColor(img_read, cv.COLOR_BGR2GRAY)) #Use gray image
@@ -71,14 +74,50 @@ def region_seg(nb=10):
         #plt.show()
 
         # Since, the contrast difference is not much. Anyways we will perform it
-        markers = np.zeros_like(img_read)
-        #markers[img_read < float(50 / 255)] = 1 # 50/255
-        markers[img_read > float(120 / 255)] = 2 # 120/255
-        print(elevation_map)
+        markers = np.zeros_like(img_read) #empty image
+        #Assign labels to each pixel in the image with respect to their intensity
+        # To choose thresholds; we compute mean and std of our train set: using 
+        # mean_dataset() and std_dataset() in help.py
+        # low_threshold = mean - k * std
+        # high_threshold = mean + k * std
+        low_th = mean_dataset(images) - k * std_dataset(images) #k = 1
+        high_th = mean_dataset(images) + k * std_dataset(images) # k = 1
+        markers[img_read < float(low_th)] = 1 
+        markers[img_read > float(high_th)] = 2 
+        #print(f"Markers: {markers}")
+        #print(elevation_map)
         #plt.imshow(markers, cmap='gray')
         #plt.title('markers')
         #plt.show()
+        
+        # Perform watershed region segmentation 
+        segmentation = watershed(elevation_map, markers)
+        #segmentation = cv.normalize(segmentation, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
+        #print(segmentation.max())
+        #cv.imwrite(os.path.join(save_path, f"watershed_seg/seg_{i}.png"), segmentation)
+        
+        #plt.imshow(segmentation)
+        #plt.title('Watershed segmentation')
+        #plt.show()
+        
+        # plot overlays and contour
+        segmentation = nd.binary_fill_holes(segmentation - 1)
+        label_rock, _ = nd.label(segmentation)
+        # overlay image with different labels
+        image_label_overlay = label2rgb(label_rock, image=img_read)
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 16), sharey=True)
+        ax1.imshow(img_read)
+        ax1.contour(segmentation, [0.8], linewidths=1.8, colors='w')
+        ax2.imshow(image_label_overlay)
+        plt.show()
+        #fig.subplots_adjust(**margins)
+        
+
 
 #canny_seg()
-region_seg()
+watershed_seg(k=1)
 #print(os.path.join(save_path, f"binary-seg/seg_{1}.tif"))
+
+# sources: https://www.geeksforgeeks.org/machine-learning/region-and-edge-based-segmentaion/
+# https://www.geeksforgeeks.org/computer-vision/image-segmentation-techniques-and-applications/
